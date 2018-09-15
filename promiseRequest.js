a4d03733765d069893315c7e6bf1b4c97b047b66
Stefan <stefan.armijo@gmail.com>
2018-09-14 19:16:59 -0700

    cleanup

--- promiseRequest.js
+++ promiseRequest.js
@@ -1,14 +1,9 @@
 const request = require('request');
-const http = require('http');
 
 const url = 'https://headlight-tournament-2.herokuapp.com';
 
 const data = {
   callsign: 'XelpmisFree',
-};
-
-const cb = function (response) {
-  console.log('Here is my response: ', response);
 };
 
 const promiseRequest = endPoint => new Promise((resolve, reject) => {
