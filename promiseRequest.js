ff920bad7da95bf96404173856741d595bd03a5f
Stefan <stefan.armijo@gmail.com>
2018-09-14 19:11:50 -0700

    implement post request using request library

--- promiseRequest.js
+++ promiseRequest.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const http = require('http');
 
+const url = 'https://headlight-tournament-2.herokuapp.com';
+
 const data = {
   callsign: 'XelpmisFree',
 };
@@ -9,40 +11,14 @@ const cb = function (response) {
   console.log('Here is my response: ', response);
 };
 
-request.post({ url: 'https://headlight-tournament-2.herokuapp.com/register', form: JSON.stringify(data) }, (err, res, body) => {
-  if (err) {
-    console.log('ERROR: ', err);
-  } else {
-    console.log('body: ', JSON.parse(body));
-  }
+const promiseRequest = endPoint => new Promise((resolve, reject) => {
+  request.post({ url: `${url}${endPoint}`, form: JSON.stringify(data) }, (err, res, body) => {
+    if (err) {
+      console.log('Error: ', err);
+      reject(err);
+    }
+    resolve(JSON.parse(body));
+  });
 });
 
-function SendPOSTRequest(path, data, callback) {
-  const options = {
-    hostname: 'headlight-tournament-3.herokuapp.com',
-    port: 80,
-    path,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  const response = [];
-
-  const req = http.request(options, (res) => {
-    res.on('data', (chunk) => { response.push(chunk); });
-    res.on('end', () => {
-      const finalResponse = JSON.parse(response.join(''));
-      callback(finalResponse);
-    });
-  });
-
-  req.write(JSON.stringify(data));
-  req.end();
-}
-
-// Sample request
-
-
-// SendPOSTRequest('/register', data, cb);
+module.exports = { promiseRequest };
